0d644e25c734b74dc844f8ac5fb9c963979e279e
sharlq <shehabakhader@gmail.com>
2021-05-03 02:57:04 +0300

    first project done

--- banking system/banking app.py
+++ banking system/banking app.py
@@ -1,4 +1,4 @@
-class user():
+class User():
    def __init__(self,name,age,gender):
        self.name = name
        self.age = age
@@ -11,5 +11,32 @@ class user():
        print("Age " , self.age)
        print("Gender " ,self.gender)
 
-johan = user('johan',21,'mal')
-johan.show_Detailes()
+class Bank(User):
+    def __init__(self,name,age,gender):
+        super().__init__(name,age,gender)
+        self.balance = 0
+
+    def deposit(self,amount):
+        self.balance = self.balance + amount
+        print("Accuont balance has been updated : $",self.balance)
+
+    def withdraw(self,amount):
+        if amount > self.balance:
+            print("insufficient Funds ! Balance Available : $",self.balance)
+        else:
+            self.balance = self.balance - amount
+            print("Accuont balance has been updated : $",self.balance)
+    def view_balance(self):
+        self.show_Detailes()
+        print("Accuont balance is : $",self.balance)
+
+
+jo = Bank("jo",21,"male")
+jo.deposit(200)
+jo.deposit(10)
+jo.withdraw(5)
+jo.withdraw(2000)
+jo.view_balance()
+
+
+
