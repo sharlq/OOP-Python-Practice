9a89f1b9dd5c258e500b79c102d6d5796d3edb37
sharlq <shehabakhader@gmail.com>
2021-05-04 15:39:17 +0300

    add features to the LinkedList

--- Linked list/LinkedList.py
+++ Linked list/LinkedList.py
@@ -44,8 +44,40 @@ class Linked_List:
             elements.append(current_node.data)
         print(elements)
 
+    def get(self,index):
+        if index>=self.length():
+            print ("ERrOR: 'Get' out of range!")
+            return None
+        current_index = 0
+        current_node = self.head
+        while True:
+            current_node = current_node.next
+            if current_index==index:
+                return current_node.data
+            current_index += 1
+    # note here we can get element by index but it takes O(n) time
+      
+    def erase(self,index):
+        if index>=self.length():
+            print("ERROR: 'Erase' out of range!")
+            return
+        current_index=0
+        current_node = self.head
+        while True:
+            last_node = current_node
+            current_node = current_node.next
+            if current_index==index:
+                last_node.next = current_node.next
+                return
+            current_index+=1
 
 my_list = Linked_List()
-
+my_list.display()
 my_list.append(1)
+my_list.append(3)
+my_list.append(5)
+my_list.append(2)
+my_list.display()
+print(f"element at second index {my_list.get(2)}")
+my_list.erase(2)
 my_list.display()
